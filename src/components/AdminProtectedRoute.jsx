cb64cd2f019f2bf8cb19fed48d8cfd80334a3ca0
snshah55982 <shivnarayan2080.com>
2025-07-01 23:23:39 +0545

    changes

--- src/components/AdminProtectedRoute.jsx
+++ src/components/AdminProtectedRoute.jsx
@@ -4,8 +4,6 @@ import { Navigate } from "react-router";
 const AdminProtectedRoute = ({ children }) => {
   const { user } = useSelector((state) => state.userSlice);
 
-  console.log(user);
-
   if (!user || user?.role !== "admin") {
     return <Navigate to={"/login"} replace={true} />;
   }

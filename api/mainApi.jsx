2ec9a345afe6b682654b7717b8a895f2812e70c7
snshah55982 <shivnarayan2080.com>
2025-07-01 22:39:18 +0545

    second change

--- api/mainApi.jsx
+++ api/mainApi.jsx
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-export const BASE_URL = "http://localhost:5000";
+export const BASE_URL = "http://192.168.106.11:5000";
 
 export const mainApi = createApi({
   reducerPath: "mainApi",
